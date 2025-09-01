341fa3aed36e7872a5c0ff81d4068f17ccb0f069
nick-hue <nickagelid@gmail.com>
2025-09-01 23:15:41 +0300

    added warning remover

---
+++ src/third_party/raygui_impl.cpp
@@ -0,0 +1,10 @@
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wmissing-field-initializers"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wunused-result"  // for fgets/fread warnings
+
+#define RAYGUI_IMPLEMENTATION
+#include "raygui.h"
+
+#pragma GCC diagnostic pop
